b74a669016d7c999aaa94a613413f2262f48153a
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2024-12-30 14:40:56 +0200

    make build

---
+++ index.a5f3a367.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=document.querySelector(".spider");t.style.top=e.clientHeight/2-t.clientHeight/2+"px",t.style.left=e.clientWidth/2-t.clientWidth/2+"px";
+//# sourceMappingURL=index.a5f3a367.js.map
